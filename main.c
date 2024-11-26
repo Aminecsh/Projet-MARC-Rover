d2ed9a42694d6f07d9a98b956c8c63e6d5e06014
ArthurD0 <131153831+ArthurD0@users.noreply.github.com>
2024-11-27 00:21:18 +0100

    Update main.c

--- main.c
+++ main.c
@@ -10,7 +10,7 @@ int main() {
     // Charger la carte
     t_map carte = creerCarteDepuisFichier("maps-backup/example1.map");
 
-    // Définir la position de départ et l'orientation de MARC
+    // Définir la position de départ et l'orientation  de MARC
     // Départ au coin inférieur gauche face au NORD
     t_position pos_depart = {0, carte.y_max - 1};
     t_localisation loc_depart = loc_init(pos_depart.x, pos_depart.y, NORD);
